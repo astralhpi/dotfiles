858c9abb6b7f681f085d41d494618b2dc89b4714
Song Jaehak <jaehak@hpi.cc>
2016-12-06 15:00:46 +0000

    change ssh-key if only file not exists

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,17 @@
 #/bin/bash
 
-ssh-keygen -t rsa -b 4096 -C "jaehak@hpi.cc"
+if [ ! -e $HOME/.ssh/id_rsa.pub ];
+then
+    ssh-keygen -t rsa -b 4096 -C "jaehak@hpi.cc"
+fi
 
 # check OS
 OS=$(lsb_release -si)
 
 if [ OS='Ubuntu' ];
 then
+    add-apt-repository ppa:neovim-ppa/unstable
+    apt-get update
     cat ./packages/apt-packages | xargs apt-get install
 fi
 
