7bf7663271665167bee9163947a1dce8559562b9
송재학 <jaehak@jaehak-macbook-pro.local>
2016-12-16 00:35:32 +0900

    delete apt-packages and add brew install to boostrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,9 @@ then
     ssh-keygen -t rsa -b 4096 -C "jaehak@hpi.cc"
 fi
 
-# check OS
-OS=$(lsb_release -si)
-
-if [ OS='Ubuntu' ];
-then
-    add-apt-repository ppa:neovim-ppa/unstable
-    apt-get update
-    cat ./packages/apt-packages | xargs apt-get install
+if ! type "brew" > /dev/null; then
+    echo "Install Homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+brew tap Homebrew/bundle
