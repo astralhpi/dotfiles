d3003823239668ce4f46d3470552d80be38bf1de
Song Jaehak <master@hpi.cc>
2016-12-16 14:49:25 +0900

    rename cmd_exists to exists_cmd in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,21 @@
 #/bin/bash
 
-function exists_cmd() {
+function cmd_exists() {
     local cmd=$1
-    type "$cmd" > /dev/null
+    type "$cmd" > /dev/null ;
 }
 
 if [ ! -e $HOME/.ssh/id_rsa.pub ]; then
     ssh-keygen -t rsa -b 4096 -C "jaehak@hpi.cc"
 fi
 
-if ! (exists_cmd "brew"); then
+if ! cmd_exists brew; then
     echo "Installing Homebrew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     echo "Installed Homebrew"
 fi
 
-if ! (exists_cmd "mac"); then
+if ! cmd_exists mac; then
     echo "Installing mac-cli"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/guarinogabriel/mac-cli/master/mac-cli/tools/install)"
     echo "Installed mac-cli"
