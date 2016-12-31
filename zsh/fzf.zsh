3044eea7d95d23b6c1dc1b2cd5b9c7fbad0d141c
Song Jaehak <jaehak@hpi.cc>
2016-12-31 18:14:32 +0900

    Fzf: Update config file

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,20 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/Cellar/fzf/0.15.9/bin* ]]; then
-  export PATH="$PATH:/usr/local/Cellar/fzf/0.15.9/bin"
-fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" == */usr/local/Cellar/fzf/0.15.9/man* && -d "/usr/local/Cellar/fzf/0.15.9/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/Cellar/fzf/0.15.9/man"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.15.9/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/Cellar/fzf/0.15.9/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
 
