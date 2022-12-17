2edec1163cf397d5850deb65c0ee0f855c8c4a43
Jake(Jaehak Song) <jake@heybit.io>
2022-12-18 03:12:10 +0900

    feat: hammerspoon 설정 몇가지 추가

---
+++ hammerspoon/init.lua
@@ -0,0 +1,32 @@
+hs.hotkey.bind({"cmd", "ctrl"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+
+local inputEnglish = "com.apple.keylayout.ABC"
+local esc_bind
+
+function convert_to_eng_with_esc()
+	local inputSource = hs.keycodes.currentSourceID()
+	if not (inputSource == inputEnglish) then
+		hs.eventtap.keyStroke({}, 'right')
+		hs.keycodes.currentSourceID(inputEnglish)
+	end
+	esc_bind:disable()
+	hs.eventtap.keyStroke({}, 'escape')
+	esc_bind:enable()
+end
+
+esc_bind = hs.hotkey.new({}, 'escape', convert_to_eng_with_esc):enable()
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
