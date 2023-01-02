49e586cb81b62f5d76f83f6600b6533bf91d4c9a
Jake(Jaehak Song) <jake@heybit.io>
2023-01-02 17:52:02 +0900

    chore: 주석 추가

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -6,6 +6,7 @@ local M = {}
 M.ui = {
   theme = "chadracula",
   hl_add = {
+    -- Diagnostic
     DiagnosticUnderlineError = { undercurl = true, sp = "#ff7070" },
     DiagnosticUnderlineWarn = { undercurl = true, sp = "#FFB86C" },
     DiagnosticUnderlineInfo = { undercurl = true, sp = "#a1b1e3" },
