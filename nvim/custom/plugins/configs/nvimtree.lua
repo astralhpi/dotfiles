77dbfab1f6664041319636f6514fbd58846bb9d2
Jake(Jaehak Song) <jake@heybit.io>
2023-02-10 14:57:55 +0900

    chore: deprecated 된 설정 제거

--- nvim/custom/plugins/configs/nvimtree.lua
+++ nvim/custom/plugins/configs/nvimtree.lua
@@ -11,7 +11,6 @@ local options = {
   sort_by = "case_sensitive",
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
   ignore_ft_on_setup = { "alpha" },
   hijack_cursor = true,
   hijack_unnamed_buffer_when_opening = false,
