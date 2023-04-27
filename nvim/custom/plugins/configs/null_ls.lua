53c41f829cbc4b8017e76cd9b36426f77bb729d9
Jake(Jaehak Song) <jake@heybit.io>
2023-04-27 17:59:01 +0900

    feat: python linter ruff로 변경

--- nvim/custom/plugins/configs/null_ls.lua
+++ nvim/custom/plugins/configs/null_ls.lua
@@ -13,7 +13,8 @@ local options = {
 
     -- python
     null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.formatting.ruff,
+    null_ls.builtins.diagnostics.ruff,
 
     -- rust
     null_ls.builtins.formatting.rustfmt,
