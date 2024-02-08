d39f94f9b0f7c5d478d3004040bf444d7e7e7253
Jake(Jaehak Song) <jake@heybit.io>
2024-02-08 21:21:17 +0900

    feat: wezterm 개선

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,6 +5,29 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Dracula (Official)'
 config.use_fancy_tab_bar = false
 config.window_decorations = "RESIZE"
-config.font = wezterm.font("Liga SFMono Nerd Font")
+
+local font_family = "Liga SFMono Nerd Font"
+
+
+config.font = wezterm.font_with_fallback {
+  { family = font_family }
+}
+
+
+local font_rules = {}
+for _, italic in ipairs({ false, true }) do
+  for intensity, weight in pairs({ Half = "Light", Normal = "Regular", Bold = "Bold" }) do
+    font_rules[#font_rules + 1] = {
+      italic = italic,
+      intensity = intensity,
+      font = wezterm.font_with_fallback {
+        { family = font_family, italic = italic, weight = weight }
+      }
+    }
+  end
+end
+
+config.font_rules = font_rules
+config.font_size = 12.0
 
 return config
