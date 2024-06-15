0933f5035192e4a4873bc7b032a918bbcc472add
Jake(Jaehak Song) <jake@heybit.io>
2024-06-15 18:51:28 +0900

    feat: db IDE 추가

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -53,5 +53,22 @@ return {
         }
       })
     end
-  }
+  },
+  -- DB IDE
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = { "Dbee" },
+    build = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup( --[[optional config]])
+    end,
+  },
 }
